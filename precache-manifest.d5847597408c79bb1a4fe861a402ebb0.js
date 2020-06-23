273dc4aefa460723f943c9254326bfe80b8fc2d4
Pawel <d@browa.co.uk>
2020-06-23 09:31:44 +0100

    Updates

---
+++ precache-manifest.d5847597408c79bb1a4fe861a402ebb0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2ea778080a02d147c733f068fbd29ba",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "8f2ed6fc6152aac77bab",
+    "url": "/react-weather-app/static/css/2.f21ddc88.chunk.css"
+  },
+  {
+    "revision": "9c9686333783374102cd",
+    "url": "/react-weather-app/static/css/main.5909eaf1.chunk.css"
+  },
+  {
+    "revision": "8f2ed6fc6152aac77bab",
+    "url": "/react-weather-app/static/js/2.5d0e6a33.chunk.js"
+  },
+  {
+    "revision": "0b8139a8755d9e3a5a58facd1a683833",
+    "url": "/react-weather-app/static/js/2.5d0e6a33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c9686333783374102cd",
+    "url": "/react-weather-app/static/js/main.2221796d.chunk.js"
+  },
+  {
+    "revision": "3df536e26558fa0234b6",
+    "url": "/react-weather-app/static/js/runtime-main.5cf590e3.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-weather-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-weather-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-weather-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-weather-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-weather-app/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "69b21a1ee077b51d15a72e1aab97c2d1",
+    "url": "/react-weather-app/static/media/sunshine.69b21a1e.jpg"
+  },
+  {
+    "revision": "4817263fbf37d4ccfa35e657c34a017c",
+    "url": "/react-weather-app/static/media/winter.4817263f.jpg"
+  }
+]);
